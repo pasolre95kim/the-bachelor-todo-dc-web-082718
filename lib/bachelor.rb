9925cfde45adac78ac685b45447a8a16b419bc68
Michelle Kim <pasolre95kim@gmail.com>
2018-08-25 21:03:15 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,63 @@
+require "pry"
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |people|
+
+     people.each do |k,v|
+       if v == "Winner"
+           full_name = people["name"]
+           return full_name.split(' ').first
+       end
+     end
+   end
 end
+
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, array|
+  array.each do |people|
+    people.each do |k,v|
+      if v == occ  # code here
+  data.each do |season, array|
+    array.each do |hash|
+      hash.each do |k,v|
+        if v == hometown
+          return hash["occupation"]
+        end
+      end
+    end
+  endupation
+        return people["name"]
+      end
+    end
+  end
+end
 end
 
+
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+  binding.pry
+  data.each do |season, array|
+    array.each do |hash|
+      hash.each do |k,v|
+        if v == hometown
+          counter += 1
+        end
+      end
+    end
+  end
+counter
+
+end
 end
 
 def get_occupation(data, hometown)
-  # code here
+
 end
 
 def get_average_age_for_season(data, season)
   # code here
 end
+
+
