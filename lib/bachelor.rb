c653cf760699801123b1d323285119351eee4193
Michelle Kim <pasolre95kim@gmail.com>
2018-08-25 21:03:26 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -17,16 +17,7 @@ def get_contestant_name(data, occupation)
   data.each do |season, array|
   array.each do |people|
     people.each do |k,v|
-      if v == occ  # code here
-  data.each do |season, array|
-    array.each do |hash|
-      hash.each do |k,v|
-        if v == hometown
-          return hash["occupation"]
-        end
-      end
-    end
-  endupation
+      if v == occupation
         return people["name"]
       end
     end
@@ -53,11 +44,18 @@ end
 end
 
 def get_occupation(data, hometown)
-
+  # code here
+  data.each do |season, array|
+    array.each do |hash|
+      hash.each do |k,v|
+        if v == hometown
+          return hash["occupation"]
+        end
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
   # code here
 end
-
-
